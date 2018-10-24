c5fc1daea031286dc18eab350278c54d9dd3e02a
Ryan Eakins <ryanoftinellb@gmail.com>
2018-10-24 15:03:24 +1100

    Revert changes to search.js

--- search.js
+++ search.js
@@ -4,24 +4,23 @@ if (window.location.href.indexOf("?") != -1) {
 
 function search() {
     document.getElementById("results").innerHTML = "Searching...";
-    var url = "data.json";
+    var url = "searching.json";
     var xmlhttp = new XMLHttpRequest();
     var andButton = document.getElementById("and")
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(JSON.parse(this.responseText));
-            // var text = JSON.parse(this.responseText);
-            // var terms = getTerms();
-            // if (!terms.length) {
-            //     arr = [];
-            // } else if (terms.length == 1) {
-            //     arr = oneTermSearch(text, terms);
-            // } else if (andButton.checked) {
-            //     arr = andSearch(text, terms);
-            // } else {
-            //     arr = orSearch(text, terms);
-            // }
-            // display(arr, text, "results", terms);
+            var text = JSON.parse(this.responseText);
+            var terms = getTerms();
+            if (!terms.length) {
+                arr = [];
+            } else if (terms.length == 1) {
+                arr = oneTermSearch(text, terms);
+            } else if (andButton.checked) {
+                arr = andSearch(text, terms);
+            } else {
+                arr = orSearch(text, terms);
+            }
+            display(arr, text, "results", terms);
         }
     };
     xmlhttp.open("GET", url, true);
